d2ac1793f1f5e5d006e580377c12dfc7d3fdf035
bhanuaswi-123 <bhanuaswi@gmail.com>
2021-05-10 16:20:02 +0530

    Modified Authentication css

--- project_wad/settings.py
+++ project_wad/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'project_wad.wsgi.application'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="bhagyasreerachamalla@gmail.com"
-EMAIL_HOST_PASSWORD="9390491781"
+EMAIL_HOST_USER="your email"
+EMAIL_HOST_PASSWORD="your password"
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 
 # Database
